597b21209b15e6fbd90b9be4d3f239929d7999bd
WipadaW <wipadaw@uw.edu>
2017-12-14 00:06:09 -0800

    Edit SVM

--- R/Technology_Sector_Analysis_SVM.R
+++ R/Technology_Sector_Analysis_SVM.R
@@ -19,49 +19,59 @@ N <- nrow(logistic_xts)
 N_train <- 0.9*N
 N_test <- 0.1*N
 cost <- 10
-gamma <- 0.01
+gamma <- 1
 train_set <- logistic_xts[1:N_train, ]
 test_set <- logistic_xts[(N_train+1):N, ]
-svm_radial <- svm(beat_mkt ~ ., data = train_set,
-    kernel = "radial", cost = cost, gamma = gamma)
 
-svm_predtrain <- predict(svm_radial, train_set)
-train_prediction <- as.numeric(svm_predtrain > 0.5)
-train_accuracy <- sum(as.numeric(train_prediction == train_set$beat_mkt))/N_train
+tmp_train <- train_set
+tmp_test <- test_set
+
+train_set$beat_mkt <- factor(train_set$beat_mkt, 
+                             levels = c(0, 1) , 
+                             labels = c("0", "1")) # Factor 1 == loss, 2 == beat
+
+x <- coredata(subset(train_set, select=-beat_mkt))
+y <- coredata(subset(train_set, select=beat_mkt))
+svm_radial <- svm(y = y, x = x, scale = F, 
+                  type = "C-classification",
+                  cost = 2, gamma = 10)
+
+svm_predtrain <- predict(svm_radial, x)
+
+train_accuracy <- sum(as.numeric(svm_predtrain == coredata(train_set$beat_mkt)))/N_train
 train_accuracy
 
-svm_predtest <- predict(svm_radial, test_set)
-test_prediction <- as.numeric(svm_predtest > 0.5)
-test_accuracy <- sum(as.numeric(test_prediction == test_set$beat_mkt))/(N_test)
-test_accuracy
+test_set$beat_mkt <- factor(test_set$beat_mkt, 
+                             levels = c(0, 1) , 
+                             labels = c("0", "1")) # Factor 1 == loss, 2 == beat
 
-plot(y = test_set$beat_mkt, 
-     x = as.Date(index(test_set)), 
-     typ = "l", ylim = c(min(0, svm_predtest), max(1, svm_predtest) + 0.1), 
-     xlab = "Date", ylab = "Probability", main = "SVM Prediction on test set(80/20)")
-grid()
-points(y = svm_predtest,  
-       x = as.Date(index(test_set)), 
-       col = "red")
-text(x = as.Date(index(test_set)[N_test/2]), 
-     y = max(1, svm_predtest) + 0.1,
-     labels = paste0("Train accuracy = ", round(train_accuracy, 5),
-                     " Test accuracy = ", round(test_accuracy, 5)), 
-     col = "blue")
-lines(y = rep(0.5, N_test), x = as.Date(index(test_set)), col = "green")
+x <- coredata(subset(test_set, select=-beat_mkt))
+y <- coredata(subset(test_set, select=beat_mkt))
+
+svm_predtest <- predict(svm_radial, x)
+
+test_accuracy <- sum(as.numeric(test_prediction == coredata(test_set$beat_mkt)))/(N_test)
+test_accuracy
 
 ###########################################################################
 
+factor_xts <- logistic_xts
+factor_xts$beat_mkt <- factor(factor_xts$beat_mkt, 
+                              levels = c(0, 1) , 
+                              labels = c("0", "1")) # Factor 1 == loss, 2 == beat
+x <- coredata(subset(factor_xts, select=-beat_mkt))
+y <- coredata(subset(factor_xts, select=beat_mkt))
 #find optimal parameters in a specified range
-tune_out <- tune.svm(beat_mkt ~ ., data = logistic_xts,
+tune_out <- tune.svm(x = x, y = y,
                      gamma=10^(-3:3),
                      cost=c(0.01,0.1,1,10,100,1000),
-                     kernel="radial")
+                     kernel="radial",
+                     scale = F)
 best_C <- tune_out$best.parameters$cost
 best_gamma <- tune_out$best.parameters$gamma
 
 svm_model_kernel <- function(input_xts, train_windows, predict_window, 
-                             kernel = "radial", cost = 1, gamma = 0.1){
+                             kernel = "radial", cost = 10, gamma = 1){
   N <- nrow(input_xts)
   result_set <- NULL
   
@@ -71,18 +81,20 @@ svm_model_kernel <- function(input_xts, train_windows, predict_window,
     test_set <- input_xts[(last_index+1):(last_index+predict_window), ]
     
     svm_model <- svm(beat_mkt ~ ., data = train_set,
-                      kernel = kernel, cost = cost, gamma = gamma)
+                      kernel = kernel, 
+                     cost = cost, 
+                     gamma = gamma,
+                     scale = F, 
+                     type = "C-classification")
     svm_predtest <- predict(svm_model, test_set)
-    test_value <- as.numeric(svm_predtest)
     
     tmp <- data.frame(as.data.frame(index(test_set)), 
-                      as.data.frame(test_value), 
+                      as.data.frame(svm_predtest), 
                       as.data.frame(coredata(test_set[, "beat_mkt"])))
     colnames(tmp) <- c("Date", "fitted", "actual")
     result_set <- rbind(result_set, tmp)
   }
-  predicted_values <- as.numeric(result_set[, "fitted"] > 0.5)
-  accuracy <- sum(!xor(result_set[, "actual"], predicted_values))/length(predicted_values)
+  accuracy <- sum((result_set[, "actual"] == result_set[, "fitted"]))/nrow(result_set)
   
   return(list(
     "accuracy" = accuracy,
@@ -95,30 +107,27 @@ svm_model_kernel <- function(input_xts, train_windows, predict_window,
   ))
 }
 
-
-# train_length <- seq(from=84, to=120, by = 12)
-# predict_length <- c(1:12)
-cost_list <- c(0.01,0.1,1,10,100,1000)
-gamma_list <- 10^(-3:3)
+train_length <- seq(from=84, to=120, by = 12)
+predict_length <- c(1:6)
+cost_list <- c(0.001, 0.01, 0.1, 1, 10, 100)
+gamma_list <- c(0.001, 0.01, 0.1, 1, 10, 100)
 max_accuracy <- 0.0
 params_list <- NULL
-#for(train_windows in train_length){
-#   for(predict_window in predict_length){
-      for(C in cost_list){
-        for(gamma in gamma_list){
-          train_windows <- 120
-          predict_window <- 2
-          results <- svm_model_kernel(logistic_xts, train_windows, predict_window,
-                                      kernel = "radial", cost = C, gamma = gamma)
-    
-          if(results$accuracy > max_accuracy){
-            max_accuracy <- results$accuracy
-            params_list <- results
-          }
-        }
-      }
-#   }
-# }
+for(train_windows in train_length){
+   for(predict_window in predict_length){
+     for(cost in cost_list){
+       for(gamma in gamma_list){
+         results <- svm_model_kernel(factor_xts, train_windows, predict_window,
+                                     kernel = "radial", cost = cost, gamma = gamma)
+         
+         if(results$accuracy > max_accuracy){
+           max_accuracy <- results$accuracy
+           params_list <- results
+         }
+       }
+     }
+   }
+ }
 train_windows <- params_list$train_windows
 predict_window <- params_list$predict_window
 result_set <- params_list$result_set
